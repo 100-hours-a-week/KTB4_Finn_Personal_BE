34c4c64935e3cc098ba629814aaea1b1e61a800c
정지호 <jeongjiho@jeongjihoui-MacBookPro.local>
2026-07-26 19:27:53 +0900

    H2 설정 변견

--- src/main/java/kr/ktb/finn_week6/InitEntityConfig.java
+++ src/main/java/kr/ktb/finn_week6/InitEntityConfig.java
@@ -18,26 +18,26 @@ public class InitEntityConfig {
 
     private final UserService userService;
     private final PostService postService;
-    @Bean
-    ApplicationRunner init(){
-        return args -> initUser();
-    }
-
-
-    void initUser(){
-
-        IntStream.range(0, 10).forEach(i -> {
-
-            String email = "dummy" + i + "@gmail.com";
-            String password = "1234";
-            String nickname = "dummy" + i;
-            CreateUserCommand createUserCommand = new CreateUserCommand(nickname, email, password, null);
-            userService.register(createUserCommand);
-        });
-
-        IntStream.range(0, 10000).forEach(i -> {
-           CreatePostCommand createPostCommand = new CreatePostCommand(1L, "title" + i, "content" + i, "http://localhost:8080/images/posts/bcd32f61-58d7-441a-9e3a-830f9e891090.jpg");
-           postService.register(createPostCommand);
-        });
-    }
+//    @Bean
+//    ApplicationRunner init(){
+//        return args -> initUser();
+//    }
+//
+//
+//    void initUser(){
+//
+//        IntStream.range(0, 10).forEach(i -> {
+//
+//            String email = "dummy" + i + "@gmail.com";
+//            String password = "1234";
+//            String nickname = "dummy" + i;
+//            CreateUserCommand createUserCommand = new CreateUserCommand(nickname, email, password, null);
+//            userService.register(createUserCommand);
+//        });
+//
+//        IntStream.range(0, 10000).forEach(i -> {
+//           CreatePostCommand createPostCommand = new CreatePostCommand(1L, "title" + i, "content" + i, "http://localhost:8080/images/posts/bcd32f61-58d7-441a-9e3a-830f9e891090.jpg");
+//           postService.register(createPostCommand);
+//        });
+//    }
 }
