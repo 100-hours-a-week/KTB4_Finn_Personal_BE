40971540aa168643d288a2604540bc4f4494a8aa
정지호 <jeongjiho@jeongjihoui-MacBookPro.local>
2026-08-08 18:21:31 +0900

    perf: 게시글 좋아요 일괄 조회 쿼리 추가

--- src/main/java/kr/ktb/finn_week6/domain/like/repository/LikeRepository.java
+++ src/main/java/kr/ktb/finn_week6/domain/like/repository/LikeRepository.java
@@ -1,5 +1,6 @@
 package kr.ktb.finn_week6.domain.like.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import kr.ktb.finn_week6.domain.like.Like;
 import lombok.RequiredArgsConstructor;
@@ -7,11 +8,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
+import static kr.ktb.finn_week6.domain.like.QLike.like;
+import static kr.ktb.finn_week6.domain.post.QPost.post;
 
 @Repository
 @RequiredArgsConstructor
 public class LikeRepository {
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
 
     public void save(Like like){
         em.persist(like);
@@ -21,11 +27,16 @@ public class LikeRepository {
      return Optional.ofNullable(em.find(Like.class, id));
     }
 
-    public Optional<Like> findUndeletedByPostIdAndUserId(Long postId, Long userId){
-        return em.createQuery("SELECT l FROM Like l WHERE l.post.id = :postId AND l.user.id = :userId AND l.isDeleted = false", Like.class)
-                .setParameter("postId", postId)
-                .setParameter("userId", userId)
-                .getResultList().stream().findFirst();
+    public List<Like> findUndeletedByPostIdsAndUserId(Set<Long> postIds, Long userId){
+        return queryFactory
+                .select(like)
+                        .from(like)
+                .join(like.post, post).fetchJoin()
+                .where(
+                        like.post.id.in(postIds),
+                        like.user.id.eq(userId),
+                        like.isDeleted.eq(false)
+                ).fetch();
     }
     public Optional<Like> findByPostIdAndUserId(Long postId, Long userId){
         return em.createQuery("SELECT l FROM Like l WHERE l.post.id = :postId AND l.user.id = :userId", Like.class)
@@ -40,4 +51,14 @@ public class LikeRepository {
                 .getResultList();
     }
 
+    public Optional<Like> findUndeletedByPostIdAndUserId(Long postId, Long loginUserId) {
+        return queryFactory
+                .select(like)
+                .from(like)
+                .where(
+                        like.post.id.eq(postId),
+                        like.user.id.eq(loginUserId),
+                        like.isDeleted.eq(false)
+                ).stream().findFirst();
+    }
 }
